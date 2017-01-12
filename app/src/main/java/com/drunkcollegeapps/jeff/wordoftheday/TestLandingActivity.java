039e33757ce9216e732fabc9eb0bc3f66d51ac3b
bergej88 <bergej88@uwosh.edu>
2017-01-11 19:33:13 -0600

    Restored Changes

--- app/src/main/java/com/drunkcollegeapps/jeff/wordoftheday/TestLandingActivity.java
+++ app/src/main/java/com/drunkcollegeapps/jeff/wordoftheday/TestLandingActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,6 +24,24 @@ public class TestLandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_landing);
+
+
+        String newString;
+        if (savedInstanceState == null) {
+                Bundle extras = getIntent().getExtras();
+                if(extras == null) {
+                        newString = null;
+                    } else {
+                        newString = (String) extras.getString("word");
+                    }
+            } else {
+                newString = (String) savedInstanceState.getSerializable("word");
+            }
+
+                String word = "You selected " +
+                String.valueOf(newString);
+        Toast.makeText(TestLandingActivity.this, word, Toast.LENGTH_LONG).show();
+
 
         // -- Initialize Views -- //
         tvTest = (TextView) findViewById(R.id.tvTestInfo);
